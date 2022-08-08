141573da2d1a38740024980c453c988ded628d4b
Dishant Sharma <dishu5570@gmail.com>
2022-08-08 23:04:55 +0530

    contact us page edit

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -24,39 +24,12 @@ const ContactPage = () => {
   }
 
   return (
-    <div>
+    <div style={{zIndex:'100'}}>
       <section id="contact">
   <div className="contact-box">
-    <div className="contact-links">
+    <div style={{display:'flex', alignItems:'center', justifyContent:'center'}} className="contact-links">
       <h2>CONTACT</h2>
-      {/* <div className="links">
-        <div className="link">
-          <a>
-            <img
-              src="https://i.postimg.cc/m2mg2Hjm/linkedin.png"
-              alt="linkedin"
-            />
-          </a>
-        </div>
-        <div className="link">
-          <a>
-            <img src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github" />
-          </a>
-        </div>
-        <div className="link">
-          <a>
-            <img
-              src="https://i.postimg.cc/W4Znvrry/codepen.png"
-              alt="codepen"
-            />
-          </a>
-        </div>
-        <div className="link">
-          <a>
-            <img src="https://i.postimg.cc/NjLfyjPB/email.png" alt="email" />
-          </a>
-        </div>
-      </div> */}
+
     </div>
     <div className="contact-form-wrapper">
       <form onSubmit={handleSubmit}>
